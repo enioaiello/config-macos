9138ac53e7d4b78e408949fd5e172b15ef33ce51
Enio Aiello <aielloenio@icloud.com>
2026-02-17 19:06:26 +0100

    style: add emojis to front

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 # Vérifie si l'utilisateur a lancé le script en superutilisateur
 if [ "$EUID" -ne 0 ]; then
-    echo "Erreur : Ce script doit être exécuté avec les permissions de superutilisateur." >&2
+    echo "[❌] Erreur : Ce script doit être exécuté avec les permissions de superutilisateur." >&2
     exit 1  # Sort avec le code d'erreur 1
 fi
 
 # Vérifie si le script est utilisé sur macOS
 if [[ "$OSTYPE" != darwin* ]]; then
-    echo "Erreur : Ce script doit être exécuté sur un environnement OS X/macOS." >&2
+    echo "[❌] Erreur : Ce script doit être exécuté sur un environnement OS X/macOS." >&2
     exit 1
 fi
 
