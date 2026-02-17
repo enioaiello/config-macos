94973f054b1351db1ae9f69e9f7bdf6125c1d051
Enio Aiello <aielloenio@icloud.com>
2026-02-17 18:44:47 +0100

    feat: add instructions to check is the script is launched in macOS
    Theses instructions check if the script is launched on OS X/macOS

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ if [ "$EUID" -ne 0 ]; then
     echo "Erreur : Ce script doit être exécuté avec les permissions de superutilisateur." >&2
     exit 1  # Sort avec le code d'erreur 1
 fi
+
+# Vérifie si le script est utilisé sur macOS
+if [ "$OSTYPE" != "darwin"* ]; then
+    echo "Erreur : Ce script doit être exécuté sur un environnement OS X/macOS." >&2
+    exit 1
+fi
